3e0b3ff75f653de785e90e041eb358cb995b114f
蒼時弦也 <elct9620@frost.tw>
2020-08-02 10:53:06 +0800

    Configure provider

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { DigitaloceanProvider } from './.gen/providers/digitalocean';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    // Configure API Token
+    new DigitaloceanProvider(this, 'coscup_demo', {
+      token: process.env.DO_TOKEN
+    })
 
   }
 }
